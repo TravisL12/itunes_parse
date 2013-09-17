2664e072e631a74c9ddf7e0d1257212fd5e45b49
Travisl12 <travis.lawrence12@gmail.com>
2013-09-17 11:40:20 -0700

    WIP design

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,6 @@ post '/upload' do
   if params[:itunes_file][:type] == "text/xml"
     @all_songs = parse_songs(params[:itunes_file][:tempfile])
     flash[:success] = "Upload Success!"
-    @all_songs
     redirect '/itunes_stats'
   else
     flash[:danger] = "Please Upload an XML File"
